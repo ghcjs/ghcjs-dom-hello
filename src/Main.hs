c840394e3734afbb40e758e48bbe4b97409e77bc
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2014-09-22 02:32:10 +1200

    Enable inspector and add an event handler

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,28 @@ module Main (
     main
 ) where
 
-import GHCJS.DOM (webViewGetDomDocument, runWebGUI)
-import GHCJS.DOM.Document (documentGetBody)
-import GHCJS.DOM.HTMLElement (htmlElementSetInnerHTML)
+import Control.Applicative ((<$>))
+import Control.Monad.Trans (liftIO)
+import GHCJS.DOM
+       (enableInspector, webViewGetDomDocument, runWebGUI)
+import GHCJS.DOM.Document (documentGetBody, documentCreateElement)
+import GHCJS.DOM.HTMLElement (htmlElementSetInnerHTML, htmlElementSetInnerText)
+import GHCJS.DOM.Element (elementOnclick)
+import GHCJS.DOM.HTMLParagraphElement
+       (castToHTMLParagraphElement)
+import GHCJS.DOM.Node (nodeAppendChild)
+import GHCJS.DOM.EventM (mouseClientXY)
 
 main = runWebGUI $ \ webView -> do
+    enableInspector webView
     Just doc <- webViewGetDomDocument webView
     Just body <- documentGetBody doc
     htmlElementSetInnerHTML body "<h1>Hello World</h1>"
+    elementOnclick body $ do
+        (x, y) <- mouseClientXY
+        liftIO $ do
+            Just newParagraph <- fmap castToHTMLParagraphElement <$> documentCreateElement doc "p"
+            htmlElementSetInnerText newParagraph $ "Click " ++ show (x, y)
+            nodeAppendChild body (Just newParagraph)
+            return ()
+    return ()
