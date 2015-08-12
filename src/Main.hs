3086fc8686841afa8001218d80a9d9d54f3c8e2f
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2015-08-12 16:04:11 +1200

    Take advantange of MonadIO. Use "on doc click".

--- src/Main.hs
+++ src/Main.hs
@@ -3,12 +3,11 @@ module Main (
 ) where
 
 import Control.Applicative ((<$>))
-import Control.Monad.Trans (liftIO)
 import GHCJS.DOM
        (enableInspector, webViewGetDomDocument, runWebGUI)
-import GHCJS.DOM.Document (getBody, createElement)
+import GHCJS.DOM.Document (getBody, createElement, click)
 import GHCJS.DOM.HTMLElement (setInnerText)
-import GHCJS.DOM.Element (setInnerHTML, click)
+import GHCJS.DOM.Element (setInnerHTML)
 import GHCJS.DOM.HTMLParagraphElement
        (castToHTMLParagraphElement)
 import GHCJS.DOM.Node (appendChild)
@@ -19,11 +18,10 @@ main = runWebGUI $ \ webView -> do
     Just doc <- webViewGetDomDocument webView
     Just body <- getBody doc
     setInnerHTML body (Just "<h1>Hello World</h1>")
-    on body click $ do
+    on doc click $ do
         (x, y) <- mouseClientXY
-        liftIO $ do
-            Just newParagraph <- fmap castToHTMLParagraphElement <$> createElement doc (Just "p")
-            setInnerText newParagraph $ Just $ "Click " ++ show (x, y)
-            appendChild body (Just newParagraph)
-            return ()
+        Just newParagraph <- fmap castToHTMLParagraphElement <$> createElement doc (Just "p")
+        setInnerText newParagraph $ Just $ "Click " ++ show (x, y)
+        appendChild body (Just newParagraph)
+        return ()
     return ()
