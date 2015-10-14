2921e3edafa02b9347522f4e06366ee0e6832eef
Ken Micklas <kmicklas@gmail.com>
2015-10-14 17:47:15 -0400

    Avoid unnecessary downcast to HTMLParagraphElement

--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,6 @@ import GHCJS.DOM
        (enableInspector, webViewGetDomDocument, runWebGUI)
 import GHCJS.DOM.Document (getBody, createElement, createTextNode, click)
 import GHCJS.DOM.Element (setInnerHTML)
-import GHCJS.DOM.HTMLParagraphElement
-       (castToHTMLParagraphElement)
 import GHCJS.DOM.Node (appendChild)
 import GHCJS.DOM.EventM (on, mouseClientXY)
 
@@ -19,7 +17,7 @@ main = runWebGUI $ \ webView -> do
     setInnerHTML body (Just "<h1>Hello World</h1>")
     on doc click $ do
         (x, y) <- mouseClientXY
-        Just newParagraph <- fmap castToHTMLParagraphElement <$> createElement doc (Just "p")
+        Just newParagraph <- createElement doc (Just "p")
         text <- createTextNode doc $ "Click " ++ show (x, y)
         appendChild newParagraph text
         appendChild body (Just newParagraph)
