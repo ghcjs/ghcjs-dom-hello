5b0fb6885a7fe0d685fa96e461f20e0170e5c5b2
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-10-17 00:17:36 +1300

    Use ghcjs-dom 0.5

--- src/Main.hs
+++ src/Main.hs
@@ -6,20 +6,24 @@ import Control.Monad.IO.Class (MonadIO(..))
 import Control.Concurrent.MVar (takeMVar, putMVar, newEmptyMVar)
 
 import GHCJS.DOM (run, syncPoint, currentDocument)
-import GHCJS.DOM.Document (getBody, createElement, createTextNode)
+import GHCJS.DOM.Types
+       (HTMLParagraphElement(..), HTMLSpanElement(..), unsafeCastTo)
+import GHCJS.DOM.Document (getBodyUnsafe, createElementUnsafe, createTextNode)
 import GHCJS.DOM.Element (setInnerHTML)
 import GHCJS.DOM.Node (appendChild)
 import GHCJS.DOM.EventM (on, mouseClientXY)
 import qualified GHCJS.DOM.Document as D (click)
 import qualified GHCJS.DOM.Element as E (click)
 
-main = run 3708 $ do
+main = do
+  putStrLn "<a href=\"http://localhost:3708/\">http://localhost:3708/</a>"
+  run 3708 $ do
     Just doc <- currentDocument
-    Just body <- getBody doc
+    body <- getBodyUnsafe doc
     setInnerHTML body (Just "<h1>Kia ora (Hi)</h1>")
     on doc D.click $ do
         (x, y) <- mouseClientXY
-        Just newParagraph <- createElement doc (Just "p")
+        newParagraph <- createElementUnsafe doc (Just "p") >>= unsafeCastTo HTMLParagraphElement
         text <- createTextNode doc $ "Click " ++ show (x, y)
         appendChild newParagraph text
         appendChild body (Just newParagraph)
@@ -27,7 +31,7 @@ main = run 3708 $ do
 
     -- Make an exit button
     exitMVar <- liftIO newEmptyMVar
-    Just exit <- createElement doc (Just "span")
+    exit <- createElementUnsafe doc (Just "span") >>= unsafeCastTo HTMLSpanElement
     text <- createTextNode doc "Click here to exit"
     appendChild exit text
     appendChild body (Just exit)
