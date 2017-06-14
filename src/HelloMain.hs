8fffc5d71a5307ea4f3cc168d414516ad683ee27
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2017-06-15 10:38:03 +1200

    Update to use the latest ghcjs-dom and jsaddle

--- src/HelloMain.hs
+++ src/HelloMain.hs
@@ -5,42 +5,68 @@ module HelloMain (
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Concurrent.MVar (takeMVar, putMVar, newEmptyMVar)
 
-import GHCJS.DOM (syncPoint, currentDocumentUnchecked)
+import GHCJS.DOM
 import GHCJS.DOM.Types
-       (Element(..), HTMLParagraphElement(..),
-        HTMLSpanElement(..), uncheckedCastTo, JSM)
-import GHCJS.DOM.Document (getBodyUnsafe, createElement, createTextNode)
-import GHCJS.DOM.Element (setInnerHTML)
-import GHCJS.DOM.Node (appendChild)
-import GHCJS.DOM.EventM (on, mouseClientXY)
-import GHCJS.DOM.GlobalEventHandlers (click)
+import GHCJS.DOM.Document
+import GHCJS.DOM.Element
+import GHCJS.DOM.Node
+import GHCJS.DOM.EventM
+import GHCJS.DOM.GlobalEventHandlers
+import GHCJS.DOM.HTMLHyperlinkElementUtils
 
+-- | This is the main function of this application it is shared by all the different executable
+-- types included in this package.  Its type JSM () is like IO () but for anything that needs
+-- a JavaScript context to run.
+--
+-- One way to work on this application is to use ghci and jsaddle-warp.  Start ghci with
+-- your favourite tool, for instance from the command line you can run:
+-- > cabal new-repl
+--
+-- Make sure break-on-exception is off (otherwise it will break when we restart the warp server)
+-- > :set -fno-break-on-exception
+--
+-- There is a `.ghci` file in the ghcjs-dom-hello directory that redefines the :reload command so
+-- that each time :reload is used it will also run:
+-- > Language.Javascript.JSaddle.Warp.debug 3708 HelloMain.helloMain
+-- This will start a warp server on port 3708. If it was already running it will stop the
+-- old one first, but before it does it will send a signal to each of the connected browsers to
+-- refresh.  That way after the :reload they should all be refreshed automaticaly.
+-- (It also waits up to 10 seconds for the browsers to reconnect. If one is closed or in the
+-- or in the background you may have to wait for the ghci prompt to return).
+--
+-- Use `runOnAll` to run JavaScript on all the connected browsers.  For instance the following
+-- will print the HTML from the body of each connected browser:
+-- > Language.Javascript.JSaddle.Debug.runOnAll_ $ currentDocumentUnchecked >>= getBodyUnchecked >>= getInnerHTML >>= liftIO . putStrLn
 helloMain :: JSM ()
 helloMain = do
     doc <- currentDocumentUnchecked
-    body <- getBodyUnsafe doc
-    setInnerHTML body (Just "<h1>Kia ora (Hi)</h1>")
+    body <- getBodyUnchecked doc
+    setInnerHTML body "<h1>Kia ora (Hi)</h1>"
+
+    -- Add a mouse click event handler to the document
     _ <- on doc click $ do
         (x, y) <- mouseClientXY
         newParagraph <- uncheckedCastTo HTMLParagraphElement <$> createElement doc "p"
         text <- createTextNode doc $ "Click " ++ show (x, y)
-        _ <- appendChild newParagraph text
-        _ <- appendChild body newParagraph
-        return ()
+        appendChild_ newParagraph text
+        appendChild_ body newParagraph
 
-    -- Make an exit button
+    -- Make an exit link
     exitMVar <- liftIO newEmptyMVar
-    exit <- uncheckedCastTo HTMLSpanElement <$> createElement doc "span"
+    exit <- uncheckedCastTo HTMLAnchorElement <$> createElement doc "a"
     text <- createTextNode doc "Click here to exit"
-    _ <- appendChild exit text
-    _ <- appendChild body exit
-    _ <- on exit click $ liftIO $ putMVar exitMVar ()
+    appendChild_ exit text
+    appendChild_ body exit
+
+    -- Set an href for the link, but use preventDefault to stop it working
+    -- (demonstraights synchronous callbacks into haskell, as preventDefault
+    -- must be called inside the JavaScript event handler function).
+    setHref exit "https://github.com/ghcjs/ghcjs-dom-hello"
+    _ <- on exit click $ preventDefault >> liftIO (putMVar exitMVar ())
 
     -- Force all all the lazy JSaddle evaluation to be executed
     syncPoint
 
-    -- In GHC compiled version the WebSocket connection will end when this
-    -- thread ends.  So we will wait until the user clicks exit.
+    -- Wait until the user clicks exit.
     liftIO $ takeMVar exitMVar
-    setInnerHTML body (Just "<h1>Ka kite ano (See you later)</h1>")
-    return ()
+    setInnerHTML body "<h1>Ka kite ano (See you later)</h1>"
